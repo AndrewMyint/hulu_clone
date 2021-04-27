59f21ffc92cec29853ed3a4fb0c25f8c4af589aa
Andrew Myint <doku.potharhtoo@gmail.com>
2021-04-27 04:20:39 -0400

    ready

---
+++ components/Result.js
@@ -0,0 +1,16 @@
+import Thumbnail from "./Thumbnail"
+import FlipMove from "react-flip-move"
+
+const Result = ({ results }) => {
+  console.log("inside Result: ", results);
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4
+    3xl:flex flex-wrap justify-center ">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result}/>
+      ))}
+    </FlipMove>
+  );
+};
+
+export default Result;
