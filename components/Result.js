acb2b4edf5453976e2cf1f6b33d82f72ac6531de
Andrew Myint <doku.potharhtoo@gmail.com>
2021-08-31 13:02:20 -0400

    ready to host

--- components/Result.js
+++ components/Result.js
@@ -1,13 +1,14 @@
-import Thumbnail from "./Thumbnail"
-import FlipMove from "react-flip-move"
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 const Result = ({ results }) => {
-  console.log("inside Result: ", results);
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4
-    3xl:flex flex-wrap justify-center ">
+    <FlipMove
+      className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4
+    3xl:flex flex-wrap justify-center "
+    >
       {results.map((result) => (
-        <Thumbnail key={result.id} result={result}/>
+        <Thumbnail key={result.id} result={result} />
       ))}
     </FlipMove>
   );
